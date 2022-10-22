df020409b151eddb54371d7cb0df609dadd60fd0
Thomas Dax <thomas.dax@vivid-planet.com>
2022-10-22 15:39:02 +0200

    move feed to the index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,20 @@
 import Layout from "app/core/layouts/Layout";
 import { BlitzPage, Routes } from "@blitzjs/next";
+import { Suspense } from "react";
+import FeedList from "../app/feed/FeedList";
 
 const Home: BlitzPage = () => {
-  return <Layout title="Home">hi</Layout>;
+  return (
+    <Layout title="Home">
+      <div className="container">
+        <main>
+          <Suspense fallback="Loading...">
+            <FeedList />
+          </Suspense>
+        </main>
+      </div>
+    </Layout>
+  );
 };
 
 Home.authenticate = { redirectTo: Routes.LoginPage() };
