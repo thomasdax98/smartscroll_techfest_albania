6c63080686e05e3a86302cb44c6014c392ec1f30
Luca Pircher <luca.pricher@hotmail.com>
2022-10-22 23:38:58 +0200

    Add docker file and fly toml

--- app/feed/hooks/useCourses.ts
+++ app/feed/hooks/useCourses.ts
@@ -1,7 +1,7 @@
 import { useQuery } from "@blitzjs/rpc";
 import getCourses from "app/feed/queries/getCourses";
 
-export const useCourses = (filter: { category: string } | undefined) => {
+export const useCourses = (filter?: { category: string }) => {
   const [courses] = useQuery(getCourses, filter);
   return courses;
 };
